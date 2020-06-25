5846e6140a0e4eb672cb353d404c4262f681fee1
Amol Jain <jainamol677@gmail.com>
2020-06-25 13:40:43 +0530

    php code for task2.

---
+++ task2_code.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color: lightblue;">
+
+<h1 style="color:red;text-align:center;">My first PHP page</h1>
+
+<?php
+echo "<h2 style="color:green;>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+?>
+
+</body>
+</html> 
